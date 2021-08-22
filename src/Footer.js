f065d311cd01f0ddfaa244a43aa95a812787a713
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-22 02:01:01 -0400

    added js bootstrap bundle in index.html, changed class to className to avoid errors

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer class="footer mt-auto py-3 bg-light">
-            <div class="container">
-                <span class="text-muted">Place sticky footer content here.</span>
+        <footer className="footer mt-auto py-3 bg-light">
+            <div className="container">
+                <span className="text-muted">Place sticky footer content here.</span>
             </div>
         </footer>
     );
